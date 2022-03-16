967b1d3288d1332d048bdd18c06bc6d4000a51e7
Matt Ortner <matthewortner@gmail.com>
2022-03-15 21:46:32 -0400

    feat: add route for app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(express.static("build"));
+
+app.get("/health", (req, res) => {
   res.send("Sup?!?");
+});
+
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
